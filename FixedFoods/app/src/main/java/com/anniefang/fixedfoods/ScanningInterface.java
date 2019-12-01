8cd25402f027cff940511cd3b9ecb4c870402a6e
Annie Fang <anniefang8@gmail.com>
2019-12-01 18:56:07 -0500

    android studio project and homepage

---
+++ FixedFoods/app/src/main/java/com/anniefang/fixedfoods/ScanningInterface.java
@@ -0,0 +1,14 @@
+package com.anniefang.fixedfoods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanningInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanning_interface);
+    }
+}
