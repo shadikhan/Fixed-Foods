a9c738a00e6d068ed166aba34fc69c56cb0ee1e8
Shad Khan <shadkhan2016@gmail.com>
2019-12-03 04:06:50 -0500

    Shared Preferences Functionality for Custom

--- FixedFoods/app/src/main/java/com/anniefang/fixedfoods/CustomPage.java
+++ FixedFoods/app/src/main/java/com/anniefang/fixedfoods/CustomPage.java
@@ -2,8 +2,9 @@ package com.anniefang.fixedfoods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
@@ -12,17 +13,30 @@ import android.widget.AdapterView;
 import android.widget.TextView;
 import android.util.SparseBooleanArray;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CustomPage  extends AppCompatActivity {
 
     private ListView lv;
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private Set preferencesSet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_page);
+
+        pref = getSharedPreferences("CustomPrefs", Context.MODE_PRIVATE);
+        editor = pref.edit();
+
+        preferencesSet = pref.getStringSet("pref", null);
+
+        if (preferencesSet == null)
+            preferencesSet = new HashSet();
 
 
         String s[] = new String[]{"Apple", "Bananas", "Avocados", "Nuts", "Asparagus", "Broccoli", "Strawberries", "Grapes"};
@@ -30,17 +44,42 @@ public class CustomPage  extends AppCompatActivity {
         lv.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_multiple_choice,s));
         lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
+        loadPreferences(preferencesSet);
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
                                     long arg3) {
-                ListView lv = (ListView) arg0;
+
                 TextView tv = (TextView) lv.getChildAt(arg2);
-                String s = tv.getText().toString();
+
+                if (lv.isItemChecked(arg2)) {
+                    // Add it to our shared preferencesSet
+                    preferencesSet.add(tv.getText().toString());
+                } else {
+                    preferencesSet.remove(tv.getText().toString());
+                }
+
+//                ListView lv = (ListView) arg0;
+//                TextView tv = (TextView) lv.getChildAt(arg2);
+//                String s = tv.getText().toString();
                 //TODO: Do something after selection?
             }
         });
 
+    }
+
+    private void loadPreferences(Set preferencesSet) {
+
+        ArrayAdapter a = (ArrayAdapter) lv.getAdapter();
+
+        for (int i = 0; i < a.getCount(); i++) {
+            String s = (String) a.getItem(i);
+
+            if (preferencesSet.contains(s)){
+                lv.setItemChecked(i, true);
+            }
+        }
     }
 
     public void Save(View view) {
@@ -53,6 +92,10 @@ public class CustomPage  extends AppCompatActivity {
                 list.add(s);
             }
         }
+
+        editor.putStringSet("pref", preferencesSet);
+        editor.commit();
+
         // Go to home
         Intent intent = new Intent(getApplicationContext(), PreferencesPage.class);
         intent.putExtra("data", list);
